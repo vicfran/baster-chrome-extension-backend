855efcaeddc06ec1ffa8c26dbd2d17aa8f91f116
Victor de Francisco <victordefranciscodomingo@gmail.com>
2018-05-03 22:14:43 +0200

    First endpoint. POST request works and ProfileEntity is parsed from the request sent to ktor

--- src/main/kotlin/BasterApp.kt
+++ src/main/kotlin/BasterApp.kt
@@ -4,23 +4,39 @@ import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.CallLogging
+import io.ktor.features.Compression
+import io.ktor.features.ContentNegotiation
 import io.ktor.features.DefaultHeaders
+import io.ktor.gson.gson
+import io.ktor.request.receive
 import io.ktor.response.respondText
 import io.ktor.routing.Routing
-import io.ktor.routing.get
+import io.ktor.routing.post
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import model.ProfileEntity
 
 fun Application.module() {
     install(DefaultHeaders)
     install(CallLogging)
-    install(Routing) {
-        get("/") {
-            call.respondText { "Hello World!" }
+    install(Compression)
+    install(ContentNegotiation) {
+        gson {
+            setPrettyPrinting()
         }
     }
+    install(Routing, initRouting())
 }
 
 fun main(args: Array<String>) {
     embeddedServer(Netty, 8080, watchPaths = listOf("BasterAppKt"), module = Application::module).start(true)
+}
+
+private fun initRouting(): Routing.() -> Unit {
+    return {
+        post ("/") {
+            val params = call.receive<ProfileEntity>()
+            call.respondText { params.body }
+        }
+    }
 }
---
+++ src/main/kotlin/model/ProfileEntity.kt
@@ -0,0 +1,3 @@
+package model
+
+data class ProfileEntity(val body: String)
